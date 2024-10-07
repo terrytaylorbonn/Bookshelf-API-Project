6d375bab48282e5e1e1f5c2837ce69303bbd63d9
T Taylor <terrytaylorbonn@gmail.com>
2024-10-07 11:25:31 -0400

    L3b worked

--- src/dbService.ts
+++ src/dbService.ts
@@ -6,6 +6,11 @@ export type Book = {
     published: boolean
 }
 
+//NOT IN GIT, in video @325
+export type BookUpdate = {
+    title: string | undefined, author: string | undefined, published: boolean | undefined
+}
+
 class DBService {
     private pool = new Pool({
         connectionString: "postgresql://bookshelfDatabase_owner:sMfAYgmI9E1W@ep-silent-flower-a54xsfr6.us-east-2.aws.neon.tech/bookshelfDatabase?sslmode=require"
@@ -15,6 +20,20 @@ class DBService {
         const result = await this.pool.query("SELECT * FROM book")
         return result.rows
     }
+
+    // 1a video version
+    public async getBookById(id: number): Promise<Book> {
+        const result = await this.pool.query("SELECT * FROM book WHERE id = $1", [id])
+        return result.rows[0]
+    }
+
+    // // 1b git version
+    // public async getBookById(id: number): Promise<Book> {
+    //     const result = await this.pool.query("SELECT * FROM book WHERE id = $1", [
+    //       id,
+    //     ])
+    //     return result.rows[0] as Book
+    //   }
 
     public async insertBook(book: {
         title: string
@@ -27,6 +46,55 @@ class DBService {
         )
         return
     }
+
+    // 2a video version
+    public async updateBookById(id: number, updates: BookUpdate): Promise<Book> {
+        let query = "UPDATE book SET "
+        const keys = Object.keys(updates).filter(key => updates[key as keyof BookUpdate] !== undefined)
+        let commasNeeded = keys.length - 1
+        keys.forEach(key => {
+            query += ` ${key} = '${updates[key as keyof BookUpdate]}'`
+            if (commasNeeded > 0) {
+                query += ", "
+                commasNeeded--
+            }
+        })
+        query += `WHERE id = ${id}`
+        console.log(query)
+        await this.pool.query(query)
+        const updatedBook = await this.getBookById(id)
+        return updatedBook
+    }
+
+    // // 2b git version
+    // public async updateBookById(
+    //     id: number,
+    //     book: {
+    //       title: string | undefined
+    //       author: string | undefined
+    //       published: boolean | undefined
+    //     }
+    //   ): Promise<Book> {
+    //     let query = "UPDATE book SET "
+    //     if (book.title !== undefined) query += `title = '${book.title}', `
+    //     if (book.author !== undefined) query += `author = '${book.author}', `
+    //     if (book.published !== undefined) query += `published = ${book.published} `
+    //     query += `WHERE id = ${id}`
+    //     await this.pool.query(query)
+    //     const updatedBook = await this.getBookById(id)
+    //     return updatedBook as Book
+    //   }
+
+    // 3a video
+    public async deleteBookById(id: number): Promise<void> {
+        await this.pool.query("DELETE FROM book WHERE id = $1", [id])
+        return
+    }
+    // 3b git
+    // public async deleteBook(id: number): Promise<void> {
+    //     const result = await this.pool.query("DELETE FROM book WHERE id = $1", [id])
+    //     return
+    //   }    
 }
 export default new DBService()
 
--- src/server.ts
+++ src/server.ts
@@ -6,18 +6,27 @@ const port = 3000;
 
 app.use(express.json());
 
-const books = [
-    { title: "book1title", author: "book1author", published: true },
-    { title: "book2title", author: "book2author", published: false },
-];
+// L4 @1330 no longer needed
+// const books = [
+//     { title: "book1title", author: "book1author", published: true },
+//     { title: "book2title", author: "book2author", published: false },
+// ];
 
-app.get('/books', (req: Request, res: Response) => {
-    res.status(200).json(books);
+// old
+// app.get('/books', (req: Request, res: Response) => {
+//     res.status(200).json(books);
+// });
+
+// new my guess.. he did not shwo this in the video
+app.get('/books', async (req: Request, res: Response) => {
+    const foundBooks = await dbService.getBooks();
+    res.status(200).json(foundBooks);
 });
 
-app.get('/books/:id', (req: Request, res: Response) => {
+
+app.get('/books/:id', async (req: Request, res: Response) => {
     const { id } = req.params;
-    const foundBook = books[Number(id)];
+    const foundBook = await dbService.getBookById(Number(id));
     res.status(200).json(foundBook);
 });
 
@@ -29,19 +38,57 @@ app.post('/books', async (req: Request, res: Response) => {
     res.status(201).json(allBooks);
 });
 
-app.put('/books/:id', (req: Request, res: Response) => {
-    const { title, author, published } = req.body;
+// app.put('/books/:id', (req: Request, res: Response) => {
+//     const { title, author, published } = req.body;
+//     const { id } = req.params;
+//     const updatedBook = { title, author, published };
+//     books[Number(id)] = updatedBook;
+//     res.status(200).json(books);
+// });
+
+// 2a video version
+app.put("/books/:id", async (req: Request, res: Response) => {
+    const { title, author, published } = req.body
+    const { id } = req.params
+    const updates = { title, author, published }
+    const updatedBook = await dbService.updateBookById(Number(id), updates)
+    res.status(200).json(updatedBook)
+})
+
+
+// 2b git version
+// app.put("/books/:id", async (req: Request, res: Response) => {
+//     const { title, author, published } = req.body
+//     const { id } = req.params
+//     const updatedBook = { title, author, published }
+//     const result = await dbPrismaService.updateBookById(Number(id), updatedBook)
+//     res.status(200).json(result)
+//   })
+
+
+// old
+// app.delete('/books/:id', (req: Request, res: Response) => {
+//     const { id } = req.params;
+//     books.splice(Number(id), 1);
+//     res.status(200).json(books);
+// });
+
+// 3a video
+app.delete('/books/:id', async (req: Request, res: Response) => {
     const { id } = req.params;
-    const updatedBook = { title, author, published };
-    books[Number(id)] = updatedBook;
-    res.status(200).json(books);
+    await dbService.deleteBookById(Number(id));
+    res.status(200).send("book deleted");
 });
 
-app.delete('/books/:id', (req: Request, res: Response) => {
-    const { id } = req.params;
-    books.splice(Number(id), 1);
-    res.status(200).json(books);
-});
+
+// 3b git 
+// app.delete("/books/:id", (req: Request, res: Response) => {
+//     const { id } = req.params
+//     dbPrismaService.deleteBook(Number(id))
+//     res.sendStatus(200)
+//   })
+
+
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
