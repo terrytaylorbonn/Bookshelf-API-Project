e8dec82bc33d74b331525a2775c29e2a54daff79
T Taylor <terrytaylorbonn@gmail.com>
2024-10-07 14:23:37 -0400

    L5 worked

--- src/server.ts
+++ src/server.ts
@@ -1,51 +1,7 @@
-import express, { Request, Response } from 'express';
-import dbService from './dbService';
-import dbPrismaService from './dbPrismaService';
+import app from "./app"
 
-const app = express();
-const port = 3000;
-
-app.use(express.json());
-
-// new my guess.. he did not shwo this in the video
-app.get('/books', async (req: Request, res: Response) => {
-    const foundBooks = await dbPrismaService.getBooks();
-    res.status(200).json(foundBooks);
-});
-
-
-app.get('/books/:id', async (req: Request, res: Response) => {
-    const { id } = req.params;
-    const foundBook = await dbPrismaService.getBookById(Number(id));
-    res.status(200).json(foundBook);
-});
-
-app.post('/books', async (req: Request, res: Response) => {
-    const { title, author, published } = req.body
-    const newBook = { title, author, published }
-    const createdBook = await dbPrismaService.insertBook(newBook)
-    // await dbService.insertBook(newBook)
-    // const allBooks = await dbService.getBooks()
-    res.status(201).json(createdBook);
-});
-
-// 2a video version
-app.put("/books/:id", async (req: Request, res: Response) => {
-    const { title, author, published } = req.body
-    const { id } = req.params
-    const updates = { title, author, published }
-    const updatedBook = await dbPrismaService.updateBookById(Number(id), updates)
-    res.status(200).json(updatedBook)
-})
-
-// 3a video
-app.delete('/books/:id', async (req: Request, res: Response) => {
-    const { id } = req.params;
-    await dbPrismaService.deleteBookById(Number(id));
-    res.status(200).send("book deleted");
-});
-
+const port = 3000
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+    console.log(`Server started listening on port ${port}`)
+})
